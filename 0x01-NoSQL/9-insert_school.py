778933b8ddf5e71f02027ae8db9b20cc3cac4132
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-07-16 12:46:44 +0000

    insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection
     """
-    mongo_collection.insert(**kwargs)
+    new_documents = mongo_collection.insert_one(kwargs)
+    return new_documents.inserted_id
